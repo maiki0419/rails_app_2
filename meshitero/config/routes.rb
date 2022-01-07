d9a52504f1204ec5c79dc4957b1304ead656f14c
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-01-07 11:39:49 +0000

    [Finish]5-11

--- meshitero/config/routes.rb
+++ meshitero/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
+  devise_for :users
   root to: "homes#top"
+  get '/homes/about' => 'homes#about',as: 'about'
+  resources :post_images,only:[:new,:create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
